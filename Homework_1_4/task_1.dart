d62e4c946f45e2eb230447ed190896eb89a7ff9b
demo <bogdan7450@gmail.com>
2023-10-26 09:48:46 +0300

    Homework 1_4

---
+++ Homework_1_4/task_1.dart
@@ -0,0 +1,47 @@
+void main() {
+  Human first_human = Human("Daria", 10, 100);
+  Human second_human = Human("Oleg", 15, 140);
+  first_human.growth();
+  first_human.changeName("Mariya");
+  print(first_human.name);
+  second_human.growth();
+  second_human.changeName("Dave");
+  print(second_human.name);
+}
+
+class Human {
+  String name = "";
+  int age = 0;
+  int height = 0;
+
+  Human(this.name, this.age, this.height);
+
+  String changeName(String newName) {
+    return name = newName;
+  }
+
+  void growth() {
+    print(
+        "Людина на ім'я $name зараз $age років, та її зріст складає $height см.");
+    while (age < 96) {
+      age++;
+      if (age < 12) {
+        height += 8;
+        print(
+            "Людина на ім'я $name зараз $age років, та її зріст складає $height см.");
+      }
+      else if (age <= 25) {
+        height += 3;
+        print("Людина на ім'я $name зараз $age років, та її зріст складає $height см.");
+      }
+      else if (age < 60) {
+        print(
+            "Поки вік $name менше 60-ти років, зріст не змінюється, зараз їй $age років");
+      } else {
+        height -= 2;
+        print(
+            "$name вже старенька, їй $age років, її зріст зменшується, зараз він складає $height см");
+      }
+    }
+  }
+}
