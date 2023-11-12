38984e218b7b3caadf1c74f998928a71f738a569
demo <bogdan7450@gmail.com>
2023-11-12 21:57:38 +0200

    Homework 1_7

---
+++ homework_1_7/task_1.dart
@@ -0,0 +1,41 @@
+void main() {
+  try {
+    int numerator = 10;
+    int denominator = 2;
+
+
+    double result = divideNumbers(numerator, denominator);
+    print("Результат звичайного ділення: $result");
+
+
+    int quotient = integerDivision(numerator, denominator);
+    print("Результат цілочисельного ділення: $quotient");
+
+
+    int remainder = calculateRemainder(numerator, denominator);
+    print("Залишок від ділення: $remainder");
+  } catch (e) {
+    print("Помилка: $e");
+  }
+}
+
+double divideNumbers(int numerator, int denominator) {
+  if (denominator == 0) {
+    throw Exception("Ділення на нуль неможливе");
+  }
+  return numerator / denominator;
+}
+
+int integerDivision(int numerator, int denominator) {
+  if (denominator == 0) {
+    throw Exception("Ділення на нуль неможливе");
+  }
+  return numerator ~/ denominator;
+}
+
+int calculateRemainder(int numerator, int denominator) {
+  if (denominator == 0) {
+    throw Exception("Ділення на нуль неможливе");
+  }
+  return numerator % denominator;
+}
