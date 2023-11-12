060b168181bf508deb784cb3990a0f504d778dae
demo <bogdan7450@gmail.com>
2023-11-12 22:55:25 +0200

    commit for circle

--- homework_1_7/task_2.dart
+++ homework_1_7/task_2.dart
@@ -1,9 +1,6 @@
 import 'dart:math';
 
 void main() {
-  double radius = 5.0; // Здесь вы можете использовать ввод пользователя
-  double area = calculateCircleArea(radius);
-  print('Площадь круга с радиусом $radius равна $area');
 }
 
 double calculateCircleArea(double radius) {
